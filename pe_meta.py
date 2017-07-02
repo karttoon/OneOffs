f0421506f89d8728cb4dd5ca1579a699222f842d
karttoon <karttoon@gmail.com>
2017-07-02 19:29:09 -0400

    Update pe_meta.py

--- pe_meta.py
+++ pe_meta.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import sys, pefile
 
-__author__  = "Jeff White [karttoon]"
+__author__  = "Jeff White [karttoon] @noottrak"
 __email__   = "karttoon@gmail.com"
 __version__ = "1.0.0"
 __date__    = "10APR2017"
