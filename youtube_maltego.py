5633bcd2641c3abac064fd31fe89a59827ae0c9a
karttoon <karttoon@gmail.com>
2019-06-17 15:05:13 -0400

    Update youtube_maltego.py

--- youtube_maltego.py
+++ youtube_maltego.py
@@ -2,6 +2,11 @@
 import sys
 from googleapiclient import discovery
 
+__author__  = "Jeff White [karttoon] @noottrak"
+__email__   = "karttoon@gmail.com"
+__version__ = "1.0.0"
+__date__    = "17JUN2019"
+
 # Google API Key
 DEVELOPER_KEY = ""
 
